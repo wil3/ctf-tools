204305e155a4bde7cf6ec694e7b5d25aeaca9358
Wil <wfkoch@gmail.com>
2015-12-02 22:17:44 -0500

    waf

---
+++ mitmproxy/waf.py
@@ -0,0 +1,31 @@
+#/bin/env python
+#MITMProxy Template Script
+#flow.request.reply(KILL)
+
+
+def start(context, argv):
+    pass
+
+def clientconnect(context, ConnectionHandler):
+    pass
+
+def serverconnect(context, ConnectionHandler):
+    pass
+
+def request(context, Flow):
+    pass
+
+def responseheaders(context,Flow):
+    pass
+
+def response(Scriptcontext, Flow):
+    pass
+
+def response(Scriptcontext, Flow):
+    pass
+
+def clientdisconnect(Scriptcontext, ConnectionHandler):
+    pass
+
+def done(ScriptContext):
+    pass
